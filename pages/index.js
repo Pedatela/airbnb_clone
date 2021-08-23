93c9ab97634d992c144d872466c02f8a9da4507a
Pedatela <lucaspedatela@gmail.com>
2021-08-23 13:20:19 -0300

    Criando o largecard

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import SmallCard from '../components/SmallCard'
+import LargeCard from '../components/LargeCard'
 import MediumCard from '../components/MediumCard'
 
 
@@ -43,6 +44,12 @@ export default function Home({ exploreData, cardsData }) {
             ))}
           </div>
         </section>
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlists curated by Airbnb"
+          buttonText="Get Inspired"
+        />
       </main>
     </div>
   )
